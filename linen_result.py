52bdafdbb0f28a84134005841c25d47d41a4b428
Rachel Fuller <rlfuller@gmail.com>
2017-12-28 10:56:54 -0500

    stderr log tinkering

--- linen_result.py
+++ linen_result.py
@@ -34,7 +34,7 @@ class LinenResult(unittest.TestResult):
             report = {
                 "title": "%s: %s" %(
                     getattr(testcase, "printable_url", str(testcase)),
-                    getattr(testcase, "session_id", None)
+                    getattr(testcase, "session_id", "No session created")
                 ),
                 "value": yaml.dump(value, Dumper=self.Better,
                     allow_unicode=True, default_flow_style=False)
@@ -50,15 +50,26 @@ class LinenResult(unittest.TestResult):
         #self.failures.append((test, self._exc_info_to_string(str(err), test)))
         self.failures.append((test, str(err[1])))
         self._mirrorOutput = True
+        print(self.err_msg("FAILED", test, err), file=sys.stderr)
 
     @failfast
     def addError(self, test, err):
         """Called when an error has occurred. 'err' is a tuple of values as
         returned by sys.exc_info().
         """
-        msg = "%s: %s" % (str(err[0]), str(err[1]))
+        msg = self.err_msg(err)
         self.errors.append((test, self.truncated_str(str(err[1]))))
         self._mirrorOutput = True
+        print(self.err_msg("ERROR", test, err), file=sys.stderr)
+
+    def addSuccess(self, test):
+        msg = "%s... %s" % (str(test), "ok")
+        print(msg, file=sys.stderr)
+
+    def err_msg(self, type, test, err):
+        return "%s... %s: %s\n    %s" % (
+            str(test), type, err[0].__qualname__, str(err[1])
+        )
 
     def truncated_str(self, str):
         if len(str) > self.truncation_threshold:
